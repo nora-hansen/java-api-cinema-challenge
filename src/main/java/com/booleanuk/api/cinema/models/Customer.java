a373a0ce2052c0067f0777bb86138d34a6dcaf2c
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-02-09 09:11:47 +0100

    Created customer model

---
+++ src/main/java/com/booleanuk/api/cinema/models/Customer.java
@@ -0,0 +1,52 @@
+package com.booleanuk.api.cinema.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+/**
+ * Class holding information about a customer
+ */
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phone;
+
+    @Column
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public Customer(
+            String name,
+            String email,
+            String phone
+            )
+    {
+        this.name  = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
